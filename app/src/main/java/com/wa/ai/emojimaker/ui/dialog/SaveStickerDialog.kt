ef586a2871a39654ad2074d2b65ed4c559db7562
Dat <datnv@wayfustudio.com>
2024-06-07 16:18:49 +0700

    Add more ads

--- app/src/main/java/com/wa/ai/emojimaker/ui/dialog/SaveStickerDialog.kt
+++ app/src/main/java/com/wa/ai/emojimaker/ui/dialog/SaveStickerDialog.kt
@@ -1,16 +1,24 @@
 package com.wa.ai.emojimaker.ui.dialog
 
-import android.graphics.Bitmap
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.ads.nativead.NativeAdView
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
 import com.wa.ai.emojimaker.R
+import com.wa.ai.emojimaker.databinding.AdNativeVideoBinding
 import com.wa.ai.emojimaker.databinding.DialogSaveBinding
 import com.wa.ai.emojimaker.ui.base.BaseBindingDialogFragment
 import com.wa.ai.emojimaker.ui.component.emojimaker.EmojiViewModel
+import com.wa.ai.emojimaker.utils.DeviceUtils
+import com.wa.ai.emojimaker.utils.RemoteConfigKey
+import com.wa.ai.emojimaker.utils.ads.NativeAdsUtils
 import com.wa.ai.emojimaker.utils.extention.setOnSafeClick
 
 class SaveStickerDialog : BaseBindingDialogFragment<DialogSaveBinding>() {
+
+    private var isLoadNativeDone = false
 
     var addToPackage: (() -> Unit)? = null
     var download: ((binding : DialogSaveBinding) -> Unit)? = null
@@ -19,8 +27,20 @@ class SaveStickerDialog : BaseBindingDialogFragment<DialogSaveBinding>() {
     override val layoutId: Int
         get() = R.layout.dialog_save
 
-
+    private val mFirebaseRemoteConfig = FirebaseRemoteConfig.getInstance()
     override fun onCreatedView(view: View?, savedInstanceState: Bundle?) {
+        if (mFirebaseRemoteConfig.getBoolean(RemoteConfigKey.IS_SHOW_ADS_NATIVE_MY_CREATIVE)) {
+            val adConfig = mFirebaseRemoteConfig.getString(RemoteConfigKey.KEY_ADS_NATIVE_MY_CREATIVE)
+            if (adConfig.isNotEmpty()) {
+                loadNativeUntilDone(adConfig)
+            }
+            else {
+                loadNativeUntilDone(getString(R.string.native_my_creative))
+            }
+        } else {
+            binding.rlNative.visibility = View.GONE
+        }
+
         setUp()
 
         val emojiViewModel : EmojiViewModel = ViewModelProvider(requireActivity())[EmojiViewModel::class.java]
@@ -30,6 +50,19 @@ class SaveStickerDialog : BaseBindingDialogFragment<DialogSaveBinding>() {
         /*if (bitmap != null) {
             binding.imgPreview.setImageBitmap(bitmap)
         }*/
+    }
+
+    private fun loadNativeUntilDone(adConfig: String) {
+        val countDownTimer: CountDownTimer = object : CountDownTimer(25000, 5000) {
+            override fun onTick(millisUntilFinished: Long) {
+                if (!isLoadNativeDone) {
+                    loadNativeAds(adConfig)
+                }
+            }
+            override fun onFinish() {
+            }
+        }
+        countDownTimer.start()
     }
 
     private fun setUp() {
@@ -51,6 +84,34 @@ class SaveStickerDialog : BaseBindingDialogFragment<DialogSaveBinding>() {
         }
         binding.bg.setOnSafeClick {
             dismiss()
+        }
+    }
+
+    private fun loadNativeAds(keyAds:String) {
+        if (!DeviceUtils.checkInternetConnection(requireContext())) binding.rlNative.visibility = View.GONE
+        this.let {
+            NativeAdsUtils.instance.loadNativeAds(
+                requireContext(),
+                keyAds
+            ) { nativeAds ->
+
+                if (nativeAds != null && isAdded && isVisible) {
+                    if (isDetached) {
+                        nativeAds.destroy()
+                        return@loadNativeAds
+                    }
+                    //binding.frNativeAds.removeAllViews()
+                    val adNativeVideoBinding = AdNativeVideoBinding.inflate(layoutInflater)
+                    NativeAdsUtils.instance.populateNativeAdVideoView(
+                        nativeAds,
+                        adNativeVideoBinding.root as NativeAdView
+                    )
+                    binding.frNativeAds.addView(adNativeVideoBinding.root)
+                    isLoadNativeDone = true
+                } else {
+                    //binding.rlNative.visibility = View.GONE
+                }
+            }
         }
     }
 }
